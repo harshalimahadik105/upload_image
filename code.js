0ac0bee8674c17996c0f16183dc5b0d726a1353a
Harshali Mahadik <harshalimahadik105@gmail.com>
2022-03-18 02:45:17 +0530

    If Else

--- code.js
+++ code.js
@@ -1,16 +1,25 @@
-function validateFileType(){
-    var upFile = document.getElementById("upFile").value;
-    var fileName = document.getElementById("fileName");
-    //alert("You selected " + upFile.split("\\").pop())
-    fileName.innerHTML="Uploaded";
-    prev_img.src = URL.createObjectURL(event.target.files[0]);
+// function validateFileType(){
+//     var upFile = document.getElementById("upFile").value;
+//     var fileName = document.getElementById("fileName");
+//     //alert("You selected " + upFile.split("\\").pop())
+//     fileName.innerHTML="Uploaded";
+//     prev_img.src = URL.createObjectURL(event.target.files[0]);
+// }
 
-    if (upFile=="jpg"){
-        //TO DO
-    }else{
-        alert("Only jpg/jpeg and png files are allowed!");
-        throw err;
+function validateFileType()
+    {
+        var typ=document.getElementById("upFile").value;
+        var res = typ.match(".jpg");
+
+        if(res)
+        {
+            // alert("sucess");
+            fileName.innerHTML="Uploaded";
+            prev_img.src = URL.createObjectURL(event.target.files[0]);
+        }
+        else
+        {
+            alert("Sorry only jpg images are accepted");
+            document.getElementById("upFile").value
+        }
     }
-  
-}
-  
